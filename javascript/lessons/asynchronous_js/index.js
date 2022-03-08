340324d24180b7e80c27dc53b30e300ef3c29b15
SedrakyanVahe <sedrakyanvahe5@gmail.com>
2022-03-08 14:18:55 +0400

    Correct syntax style

--- javascript/lessons/asynchronous_js/index.js
+++ javascript/lessons/asynchronous_js/index.js
@@ -1,27 +1,27 @@
-let name = document.getElementById("name");
-let profile_image = document.getElementById("profile_image");
-let followers = document.getElementById("followers");
+let name = document.getElementById('name');
+let profile_image = document.getElementById('profile_image');
+let followers = document.getElementById('followers');
 
 function search() {
-  let username = document.getElementById("username").value;
-  let url = "https://api.github.com/users/" + username;
+  let username = document.getElementById('username').value;
+  let url = `https://api.github.com/users/${username}`;
+
   fetch(url)
     .then((response) => {
       if (response.ok) {
         return response.json();
       }
-      throw new Error("Something went wrong");
+      throw new Error('Something went wrong');
     })
     .then(function (response) {
       name.innerText = response.name;
       profile_image.src = response.avatar_url;
-      followers.innerText = response.followers + " Followers";
+      followers.innerText = `${response.followers}  Followers`;
     })
     .catch((error) => {
       console.log(error);
-      name.innerText = "Wrong Name";
-      profile_image.src =
-        "https://library.kissclipart.com/20180902/abe/kissclipart-human-error-icon-png-clipart-computer-icons-error-c18d7a413a983be1.png";
-      followers.innerText = "0 Followers";
+      name.innerText = 'Wrong Name';
+      profile_image.src = 'https://library.kissclipart.com/20180902/abe/kissclipart-human-error-icon-png-clipart-computer-icons-error-c18d7a413a983be1.png';
+      followers.innerText = '0 Followers';
     });
 }
