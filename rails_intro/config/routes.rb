ee2d81173072f84541b0e76822f179aba79f3280
SedrakyanVahe <sedrakyanvahe5@gmail.com>
2022-04-07 13:32:20 +0400

    Add some styles

--- rails_intro/config/routes.rb
+++ rails_intro/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  
   devise_for :users
-  # root "articles#index"
   root "landing#index"
 
   resources :articles do
@@ -17,15 +17,11 @@ Rails.application.routes.draw do
 
   resources :article_arts do 
     member do
-      # patch :like 
-      # patch :dislike
-
       patch 'like', to: 'article_arts#upvote'
       patch 'unlike', to: 'article_arts#downvote'
-
     end
   end
   
   resources :users, only: [:show, :edit, :update]
-  # resources :districts
+  resources :districts
 end
