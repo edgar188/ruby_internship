04e4c6235b2d18ad0a8f88c7937bfbec04c5d495
SedrakyanVahe <sedrakyanvahe5@gmail.com>
2022-04-11 20:16:36 +0400

    Added avatar for user

--- rails_demo/app/models/user.rb
+++ rails_demo/app/models/user.rb
@@ -4,6 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
+
+  auto_strip_attributes :first_name, :last_name, :email, :phone,  squish: true
+
+  validates_presence_of :first_name, :last_name
+  validates :first_name, :last_name, length: { maximum: 255 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  
   enum gender: {
     no_select: 0,
     male: 1,
@@ -26,10 +34,4 @@ class User < ActiveRecord::Base
     buyer: 'Buyer'
   }
 
-  auto_strip_attributes :first_name, :last_name, :email, :phone,  squish: true
-
-  validates_presence_of :first_name, :last_name
-  validates :first_name, :last_name, length: { maximum: 255 }
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
-  
 end
