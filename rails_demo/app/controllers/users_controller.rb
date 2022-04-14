804da1145151b99288477ad19b8eee8c29141f8c
SedrakyanVahe <sedrakyanvahe5@gmail.com>
2022-04-14 20:54:10 +0400

    Created search method

--- rails_demo/app/controllers/users_controller.rb
+++ rails_demo/app/controllers/users_controller.rb
@@ -1,11 +1,29 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all
+    if params[:query].present?
+      @users = User.where("first_name LIKE ?", "%#{params[:query]}%")
+    else
+      @users = User.all
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
 
+  def user_params
+    params.require(:user).permit(
+      :role, 
+      :first_name, 
+      :last_name, 
+      :email, 
+      :gender, 
+      :birth_date, 
+      :country, 
+      :phone, 
+      :avatar
+      )
+  end
+    
 end
