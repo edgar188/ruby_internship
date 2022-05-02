598e747064ba3f044d6549c85b13542e2fc0e93f
SedrakyanVahe <sedrakyanvahe5@gmail.com>
2022-05-02 21:58:53 +0400

    Fixed bugs

--- rails_demo/app/models/user.rb
+++ rails_demo/app/models/user.rb
@@ -17,6 +17,7 @@ class User < ActiveRecord::Base
   validate :validate_gender, unless: -> { self.gender.nil? }
   validate :validate_birth_date, unless: -> { self.birth_date.nil? }
   validate :validate_country, unless: -> { self.country.nil? }
+  validate :validate_balance
   validate :avatar_type, if: -> { self.avatar.present? }
 
   enum role: {
