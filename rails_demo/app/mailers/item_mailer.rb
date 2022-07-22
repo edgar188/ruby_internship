f1bbf0adf3ff7625b9aeb6fddcfc99f08a9f2ddc
SedrakyanVahe <sedrakyanvahe5@gmail.com>
2022-07-22 18:11:11 +0400

    Improved app/(helpers, mailers, services) folders

--- rails_demo/app/mailers/item_mailer.rb
+++ rails_demo/app/mailers/item_mailer.rb
@@ -3,10 +3,10 @@ class ItemMailer < ApplicationMailer
   def item_created
     @user = params[:user]
     @item = params[:item]
-    
+
     mail(
-      to: ENV['MAIL_TO'], 
-      cc: AdminUser.all.pluck(:email), 
+      to: ENV['MAIL_TO'],
+      cc: AdminUser.all.pluck(:email),
       subject: 'New item created'
     )
   end
